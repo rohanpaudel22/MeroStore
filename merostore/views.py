d9d37bfd0c4d29cdd4215995cdaa3b0b3b816b8a
rohanpaudel22 <poudelrohan2060@gmail.com>
2026-03-23 23:25:16 +0545

    Fixed UnboundLocalError in home view

--- merostore/views.py
+++ merostore/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from store.models import Product ,ReviewRating
 
-def home(request):
-  products = Product.objects.all().filter(is_available = True).order_by('created_date')
+# def home(request):
+#   products = Product.objects.all().filter(is_available = True).order_by('created_date')
   
-  for product in products:
+#   for product in products:
     
-    reviews = ReviewRating.objects.filter(product_id = product.id ,  status = True)
+#     reviews = ReviewRating.objects.filter(product_id = product.id ,  status = True)
   
-  context = {
-    'products': products,
-    'reviews' : reviews,
-  }
-  return render(request , 'home.html' , context)
+#   context = {
+#     'products': products,
+#     'reviews' : reviews,
+#   }
+#   return render(request , 'home.html' , context)
+
+from django.shortcuts import render
+from store.models import Product, ReviewRating
+
+def home(request):
+    products = Product.objects.filter(is_available=True).order_by('created_date')
+    
+    reviews = ReviewRating.objects.filter(status=True)  
+
+    context = {
+        'products': products,
+        'reviews': reviews,
+    }
+    return render(request, 'home.html', context)
